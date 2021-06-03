4268fd0651aa83257b06889558dbe608212603f7
Mohamed Keita <mohkeita13@gmail.com>
2021-06-03 17:00:20 +0200

    implement HomeScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:mediation_app_flutter/screens/details_screen.dart';
+import 'package:mediation_app_flutter/widgets/category_card.dart';
+
+import 'constants.dart';
+import 'widgets/bottom_navBar.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,22 +16,119 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Meditation App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        fontFamily: "Cairo",
+        scaffoldBackgroundColor: kBackgroundColor,
+        textTheme: Theme.of(context).textTheme.apply(displayColor: kTextColor),
       ),
-      home: Home(),
+      home: HomeScreen(),
     );
   }
 }
 
-class Home extends StatelessWidget {
-  const Home({Key key}) : super(key: key);
+class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context)
+    .size;
     return Scaffold(
+      bottomNavigationBar: BottomNavBar(),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            height: size.height * .45,
+            decoration: BoxDecoration(
+              color: Color(0xFFF5CEB8),
+              image: DecorationImage(
+                alignment: Alignment.centerLeft,
+                image: AssetImage("assets/images/undraw_pilates_gpdb.png")
+                )
+              ),
+          ),
+          SafeArea(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.topRight,
+                                    child: Container(
+                    alignment: Alignment.center,
+                    height: 52,
+                    width: 52,
+                    decoration: BoxDecoration(
+                        color: Color(0xFFF2BEA1),
+                        shape: BoxShape.circle,
+                        ),
+                        child: SvgPicture.asset("assets/icons/menu.svg"),
+                  ),
+                ),
+                Text(
+                  "Good Morning \nMohamed",
+                  style: Theme.of(context)
+                  .textTheme
+                  .display1
+                  .copyWith(fontWeight: FontWeight.w900),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 30),
+                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(29.5),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "Search",
+                        icon: SvgPicture.asset("assets/icons/search.svg"),
+                        border: InputBorder.none,
+                        ),
+                    ),
+                  ),
+                  Expanded(
+                    child: GridView.count(
+                      childAspectRatio: .85,
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 20,
+                      mainAxisSpacing: 20,
+                      children: <Widget>[
+                        CategoryCard(
+                          title: "Diet Recommendation",
+                          svgSrc: "assets/icons/Hamburger.svg",
+                          press: () {},
+                          ),
+                          CategoryCard(
+                          title: "Kegel Exercices",
+                          svgSrc: "assets/icons/Excrecises.svg",
+                          press: () {},
+                          ),
+                          CategoryCard(
+                          title: "Meditation",
+                          svgSrc: "assets/icons/Meditation.svg",
+                          press: () { 
+                            Navigator.push(context, MaterialPageRoute(builder: (context){ return DetailsScreen();}),);},
+                          ),
+                          CategoryCard(
+                          title: "Yoga",
+                          svgSrc: "assets/icons/yoga.svg",
+                          press: () {},
+                          ),
+                      ],
+                      ),
+                  )
+              ]
+            ),
+                      ),
+          )
+        ],
+        ),
       
     );
   }
 }
+
+
+
